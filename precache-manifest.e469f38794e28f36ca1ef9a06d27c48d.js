15aed8c0cc160ed5a1a1a8d56e0c00dc0c3de090
AndreiPetrusMihai <68653908+AndreiPetrusMihai@users.noreply.github.com>
2020-08-15 20:45:15 +0300

    Updates

---
+++ precache-manifest.e469f38794e28f36ca1ef9a06d27c48d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28c4485edf75cb99f5c679a5ee885a08",
+    "url": "/RoboSquad/index.html"
+  },
+  {
+    "revision": "f616b206aa6310baacc8",
+    "url": "/RoboSquad/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9fad9bdd0cb452e00b56",
+    "url": "/RoboSquad/static/css/main.2214b758.chunk.css"
+  },
+  {
+    "revision": "f616b206aa6310baacc8",
+    "url": "/RoboSquad/static/js/2.5bfb6050.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboSquad/static/js/2.5bfb6050.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fad9bdd0cb452e00b56",
+    "url": "/RoboSquad/static/js/main.5821a262.chunk.js"
+  },
+  {
+    "revision": "66b1b6d05eee266ad708",
+    "url": "/RoboSquad/static/js/runtime-main.db987346.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboSquad/static/media/SEGA.12a3db8b.woff"
+  }
+]);
